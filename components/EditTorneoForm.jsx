527a1dd85c1f48575c5fa529efe75b1ad736892e
Andres Grondona <afgo4ster@gmail.com>
2023-11-15 11:36:57 -0500

    fix-edit

--- components/EditTorneoForm.jsx
+++ components/EditTorneoForm.jsx
@@ -1,23 +1,109 @@
-"use client"
+// "use client"
 
-import { useState } from "react"
-import { useRouter } from "next/navigation"
+// import { useState } from "react"
+// import { useRouter } from "next/navigation"
 
 
 
+
+// export default function EditTorneoForm({ id, title, description }) {
+
+//   const [newTitle, setNewTitle] = useState(title)
+//   const [newDescription, setNewDescription] = useState(description)
+
+//   const router = useRouter()
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const res = await fetch(`http://localhost:3000/api/torneos/${id}`, {
+//         method: "PUT",
+//         headers: {
+//           "content-type": "application/json",
+//         },
+//         body: JSON.stringify({ newTitle, newDescription }),
+//       });
+
+//       if (!res.ok) {
+//         throw new Error("No se pudo editar el torneo");
+//       }
+
+//       router.refresh();
+//       router.push("/");
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+
+//   return (
+//     <form
+//       onSubmit={handleSubmit}
+//       className="bg-white p-8 rounded-lg max-w-lg mx-auto"
+//     >
+
+//       <h2 className="text-2xl font-bold text-gray-800 mb-6">
+//         Editar Torneo
+//       </h2>
+
+//       <div className="mb-4">
+//         <input
+//           onChange={(e) => setNewTitle(e.target.value)}
+//           value={newTitle}
+//           className="w-full rounded-lg bg-gray-100 px-4 py-2 text-gray-800"
+//           type="text"
+//           placeholder="Nombre del torneo"
+//         />
+//       </div>
+
+//       <div className="mb-4">
+//         <textarea
+//           onChange={(e) => setNewDescription(e.target.value)}
+//           value={newDescription}
+//           className="w-full rounded-lg bg-gray-100 px-4 py-2 text-gray-800"
+//           rows={4}
+//           placeholder="Descripción"
+//         />
+//       </div>
+
+//       <button
+//         type="submit"
+//         className="inline-block bg-green-500 text-white rounded-lg px-4 py-2"
+//       >
+//         Actualizar Torneo
+//       </button>
+
+//     </form>
+
+
+
+//   )
+// }
+
+
+
+//------------------------------------------------
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function EditTorneoForm({ id, title, description }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
 
-  const [newTitle, setNewTitle] = useState(title)
-  const [newDescription, setNewDescription] = useState(description)
+  const router = useRouter();
 
-  const router = useRouter()
+  const apiUrl = process.env.NODE_ENV === 'production'
+    ? "https://netwave-sigma.vercel.app"
+    : "http://localhost:3000";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await fetch(`http://localhost:3000/api/torneos/${id}`, {
+      const res = await fetch(`${apiUrl}/api/torneos/${id}`, {
         method: "PUT",
         headers: {
           "content-type": "application/json",
@@ -36,13 +122,11 @@ export default function EditTorneoForm({ id, title, description }) {
     }
   };
 
-
   return (
     <form
       onSubmit={handleSubmit}
       className="bg-white p-8 rounded-lg max-w-lg mx-auto"
     >
-
       <h2 className="text-2xl font-bold text-gray-800 mb-6">
         Editar Torneo
       </h2>
@@ -73,10 +157,6 @@ export default function EditTorneoForm({ id, title, description }) {
       >
         Actualizar Torneo
       </button>
-
     </form>
-
-
-
-  )
+  );
 }
