056502d1fc10032bb1d4b279616d4273a5ca915c
Andres Grondona <afgo4ster@gmail.com>
2023-11-15 09:48:58 -0500

    post vercel

--- app/adminTorneo/page.jsx
+++ app/adminTorneo/page.jsx
@@ -1,97 +1,214 @@
 
-"use client"
+// "use client"
 
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+// import { useState } from 'react'
+// import { useRouter } from 'next/navigation'
+
+// export default function AdminTorneo() {
+
+//     const [title, setTitle] = useState("");
+//     const [description, setDescription] = useState("");
+
+//     const router = useRouter();
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault()
+
+//         if (!title || !description) {
+//             alert("Todos los campos son obligatorios")
+//             return
+//         }
+
+//         try {
+//             const res = await fetch("http://localhost:3000/api/torneos", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify({ title, description })
+//             })
+
+//             if (res.ok) {
+//                 router.push("/")
+//             } else {
+//                 throw new Error("No se pudo crear el torneo")
+//             }
+
+
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+
+
+//     return (
+//         <div className="max-w-lg mx-auto p-8">
+//             <form
+//                 onSubmit={handleSubmit}
+//                 className="bg-white px-8 pt-6 pb-8 mb-4 rounded shadow-md"
+//             >
+//                 <div className="mb-4">
+//                     <label
+//                         className="block text-gray-700 text-lg font-bold mb-2 text-center"
+//                         htmlFor="title"
+//                     >
+//                         Nombre del Torneo
+//                     </label>
+
+//                     <input
+//                         onChange={(e) => setTitle(e.target.value)}
+//                         value={title}
+//                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                         id="title"
+//                         type="text"
+//                         placeholder="Título del torneo"
+//                     />
+//                 </div>
+
+//                 <div className="mb-4">
+//                     <label
+//                         className="block text-gray-700 text-sm font-bold mb-2 text-center"
+//                         htmlFor="description"
+//                     >
+//                         Descripción
+//                     </label>
+
+//                     <textarea
+//                         onChange={(e) => setDescription(e.target.value)}
+//                         value={description}
+//                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                         id="description"
+//                         rows="4"
+//                         placeholder="Descripción del torneo"
+//                     />
+//                 </div>
+
+//                 <button
+//                     type="submit"
+//                     className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+//                 >
+//                     Agregar Torneo
+//                 </button>
+//             </form>
+
+//         </div>
+
+//     )
+// }
+
+
+
+//-----------------------------------------------
+
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AdminTorneo() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-  
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const apiUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://netwave-sigma.vercel.app"
+      : "http://localhost:3000";
 
-        if (!title || !description) {
-            alert("Todos los campos son obligatorios")
-            return
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const res = await fetch("http://localhost:3000/api/torneos", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ title, description })
-            })
-
-            if (res.ok) {
-                router.push("/")
-            } else {
-                throw new Error("No se pudo crear el torneo")
-            }
-
-
-        } catch (error) {
-            console.log(error)
-        }
+    if (!title || !description) {
+      alert("Todos los campos son obligatorios");
+      return;
     }
 
+    try {
+      const res = await fetch(`${apiUrl}/api/torneos`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
 
-    return (
-        <div className="max-w-lg mx-auto p-8">
-            <form
-                onSubmit={handleSubmit}
-                className="bg-white px-8 pt-6 pb-8 mb-4 rounded shadow-md"
-            >
-                <div className="mb-4">
-                    <label
-                        className="block text-gray-700 text-lg font-bold mb-2 text-center"
-                        htmlFor="title"
-                    >
-                        Nombre del Torneo
-                    </label>
+      
 
-                    <input
-                        onChange={(e) => setTitle(e.target.value)}
-                        value={title}
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="title"
-                        type="text"
-                        placeholder="Título del torneo"
-                    />
-                </div>
+      if (res.ok) {
+       
+        router.push("/");
+       
+        setTitle("");
+        setDescription("");
+      } else {
+        throw new Error("No se pudo crear el torneo");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                <div className="mb-4">
-                    <label
-                        className="block text-gray-700 text-sm font-bold mb-2 text-center"
-                        htmlFor="description"
-                    >
-                        Descripción
-                    </label>
+  return (
+    <div className="max-w-lg mx-auto p-8">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white px-8 pt-6 pb-8 mb-4 rounded shadow-md"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-lg font-bold mb-2 text-center"
+            htmlFor="title"
+          >
+            Nombre del Torneo
+          </label>
 
-                    <textarea
-                        onChange={(e) => setDescription(e.target.value)}
-                        value={description}
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="description"
-                        rows="4"
-                        placeholder="Descripción del torneo"
-                    />
-                </div>
-
-                <button
-                    type="submit"
-                    className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                    Agregar Torneo
-                </button>
-            </form>
-
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="title"
+            type="text"
+            placeholder="Título del torneo"
+          />
         </div>
 
-    )
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2 text-center"
+            htmlFor="description"
+          >
+            Descripción
+          </label>
+
+          <textarea
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="description"
+            rows="4"
+            placeholder="Descripción del torneo"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Agregar Torneo
+        </button>
+      </form>
+    </div>
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
