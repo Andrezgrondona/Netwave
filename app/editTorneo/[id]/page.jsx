7387e21d18ccaf3f61dd73cb1d0d3524a99c24bc
Andres Grondona <afgo4ster@gmail.com>
2023-11-15 10:55:15 -0500

    update - vercel

--- app/editTorneo/[id]/page.jsx
+++ app/editTorneo/[id]/page.jsx
@@ -1,8 +1,43 @@
+// import EditTorneoForm from "@/components/EditTorneoForm";
+
+// const getTorneoById = async (id) => {
+//     try {
+//         const res = await fetch(`http://localhost:3000/api/torneos/${id}`, {
+//             cache: "no-store"
+//         });
+
+//         if (!res.ok) {
+//             throw new Error("Error al obtener el torneo");
+//         }
+
+//         return res.json();
+//     } catch (error) {
+//         console.log("Error al obtener el torneo", error);
+//     }
+// }
+
+
+// export default async function editTorneo({ params }) {
+//     const { id } = params;
+//     const { torneo } = await getTorneoById(id);
+//     const { title, description } = torneo;
+
+//     return (
+//         <EditTorneoForm id={id} title={title} description={description} />
+//     );
+// }
+
+//---------------------------------------------
 import EditTorneoForm from "@/components/EditTorneoForm";
+
+const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://netwave-sigma.vercel.app"
+    : "http://localhost:3000";
 
 const getTorneoById = async (id) => {
     try {
-        const res = await fetch(`http://localhost:3000/api/torneos/${id}`, {
+        const res = await fetch(`${apiUrl}/api/torneos/${id}`, {
             cache: "no-store"
         });
 
@@ -14,8 +49,7 @@ const getTorneoById = async (id) => {
     } catch (error) {
         console.log("Error al obtener el torneo", error);
     }
-}
-
+};
 
 export default async function editTorneo({ params }) {
     const { id } = params;
