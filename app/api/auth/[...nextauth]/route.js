dc0c9dfbd81ee8600bc55d14a613176dcbe6e4eb
Andres Grondona <afgo4ster@gmail.com>
2023-11-17 10:50:41 -0500

    test provider

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,23 @@
 
 
 
+// import NextAuth from "next-auth"
+// import GoogleProvider from "next-auth/providers/google";
+
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET
+//     })
+//   ]
+// })
+
+// export {handler as GET , handler as POST}
+
+
+//--------------------------
+
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
@@ -11,9 +28,17 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     })
-  ]
+  ],
+  callbacks: {
+   
+    onError: (error, _, res) => {
+      console.error("Error durante la autenticación:", error);
+      return res.status(500).end(error.message);
+    }
+  }
 })
 
-export {handler as GET , handler as POST}
+export { handler as GET, handler as POST }
+
 
 
