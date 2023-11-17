8d16ab7cf3b52c6d65369f9cb1b3a166eac81399
Andres Grondona <afgo4ster@gmail.com>
2023-11-17 11:02:57 -0500

    test provider get

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -22,23 +22,15 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     })
   ],
-  callbacks: {
-   
-    onError: (error, _, res) => {
-      console.error("Error durante la autenticación:", error);
-      return res.status(500).end(error.message);
-    }
-  }
+ 
 })
-
-export { handler as GET, handler as POST }
 
 
 
