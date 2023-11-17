271bca7e2209fd46c6c82690dfac02b6e29186eb
Andres Grondona <afgo4ster@gmail.com>
2023-11-17 11:25:34 -0500

    allowedHosts

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,20 +1,42 @@
 
+// import NextAuth from "next-auth"
+// import GoogleProvider from "next-auth/providers/google";
 
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET
+//     })
+//   ]
+// })
 
+// export {handler as GET , handler as POST}
 
-import NextAuth from "next-auth"
+//--------------------------------------------------
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    })
-  ]
-})
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    
+  ],
+
+ 
+  allowedHosts: [
+    'localhost:3000', 
+    'netwave-sigma.vercel.app', // ✅ Agregar dominio de producción
+  ],
+  
+});
 
 export {handler as GET , handler as POST}
+
+
 
 
 
