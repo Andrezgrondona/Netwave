e158e892d716570684775d016013e5c2cec0645f
Andres Grondona <afgo4ster@gmail.com>
2023-11-17 11:09:04 -0500

    auth-route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,35 +2,22 @@
 
 
 
-// import NextAuth from "next-auth"
-// import GoogleProvider from "next-auth/providers/google";
-
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET
-//     })
-//   ]
-// })
-
-// export {handler as GET , handler as POST}
-
-
-//--------------------------
-
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     })
-  ],
- 
+  ]
 })
+
+export {handler as GET , handler as POST}
+
+
+
 
 
 
